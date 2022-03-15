b5fafb63ffa96351e9eac4c180c77a750be735c3
Billions Joel <joelatugonza@gmail.com>
2022-03-15 17:24:09 +0300

    Merge sidebar with visacard

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,10 +1,12 @@
+import style from './dashboard.module.scss';
 import Sidebar from '../components/Sidebar';
 import Visa from '../components/Visa';
 
 const Dashboard = () => (
-  <>
+  <div className={style.dashboard}>
     <Sidebar />
     <Visa />
-  </>
+    <Visa />
+  </div>
 );
 export default Dashboard;

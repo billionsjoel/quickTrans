da518a34804a7b09f510670b32fe0786646e7f5f
Billions Joel <joelatugonza@gmail.com>
2022-03-15 09:34:23 +0300

    import credit visaPill

--- src/components/Visa.js
+++ src/components/Visa.js
@@ -2,7 +2,7 @@
 import style from './visa.module.scss';
 import CreditPanel from './VisaSection/CreditPanels';
 import VisaCard from './VisaSection/VisaCard';
-
+import VisaPill from './VisaSection/VisaPill';
 
 const Visa = () => (
   <div className={style.visa}>

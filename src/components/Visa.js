5a6bcae42f8edeb6479bac315bcdb2f0b0adbfae
Billions Joel <joelatugonza@gmail.com>
2022-03-15 09:34:12 +0300

    import credit visacard

--- src/components/Visa.js
+++ src/components/Visa.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-tabs */
 import style from './visa.module.scss';
 import CreditPanel from './VisaSection/CreditPanels';
-
+import VisaCard from './VisaSection/VisaCard';
 
 
 const Visa = () => (

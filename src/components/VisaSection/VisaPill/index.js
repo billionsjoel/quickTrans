bb9762c36ff7768360ae833b7a18aaf499447b44
Billions Joel <joelatugonza@gmail.com>
2022-03-15 09:43:52 +0300

    Add pill info

--- src/components/VisaSection/VisaPill/index.js
+++ src/components/VisaSection/VisaPill/index.js
@@ -4,7 +4,13 @@ import style from './style.module.scss';
 const VisaPill = () => (
   <div className={style.container}>
     <div className={style.pill}>
-     
+      <div className={style.currency}>
+        <span className={style.figure}>
+          3453
+          <span className={style.zeros}>,000</span>
+        </span>
+        <span className={style.type}>USD</span>
+      </div>
 
 
     </div>

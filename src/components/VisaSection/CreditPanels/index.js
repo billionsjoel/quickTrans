fc7bdb5baaebe112b7caf51140fe467b46736f44
Billions Joel <joelatugonza@gmail.com>
2022-03-15 10:13:26 +0300

    Import react styles

--- src/components/VisaSection/CreditPanels/index.js
+++ src/components/VisaSection/CreditPanels/index.js
@@ -3,6 +3,7 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
 import style from './style.module.scss';
+import 'react-circular-progressbar/dist/styles.css';
 
 function CreditPanel() {
   return (

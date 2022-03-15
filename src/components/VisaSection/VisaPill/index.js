09194f5809fb9f79280854991c528da8cf31b99b
Billions Joel <joelatugonza@gmail.com>
2022-03-15 09:43:33 +0300

    Add visa pill style

--- src/components/VisaSection/VisaPill/index.js
+++ src/components/VisaSection/VisaPill/index.js
@@ -4,20 +4,11 @@ import style from './style.module.scss';
 const VisaPill = () => (
   <div className={style.container}>
     <div className={style.pill}>
-      <div className={style.currency}>
-        <span className={style.figure}>
-          3453
-          <span className={style.zeros}>,000</span>
-        </span>
-        <span className={style.type}>USD</span>
-      </div>
+     
 
-      <div className={style.more}>
-        <span>...5460</span>
-        <RiVisaLine className={style.set} />
-      </div>
+
     </div>
-    <div className={style.notification}> 2 </div>
+
   </div>
 );
 export default VisaPill;
